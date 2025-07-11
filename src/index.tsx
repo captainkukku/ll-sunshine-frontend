f16cb64e5d77a6a43e2845158e85867f0cf76f07
captainkukku <natsu.217828@gmail.com>
2025-07-12 06:51:54 +0900

    feat: support offline image uploads

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/theme.css';
 import 'leaflet/dist/leaflet.css';
+import { processPendingUploads } from './utils/compareImageManager';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(<App />);
+
+processPendingUploads();
+window.addEventListener('online', processPendingUploads);
 
 
